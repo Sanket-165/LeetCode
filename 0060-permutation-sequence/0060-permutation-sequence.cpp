dc5302f7b960d0e926262e4078ce28a5da4a6b66
Sanket Gavhane <166587893+Sanket-165@users.noreply.github.com>
2026-07-26 21:26:24 +0530

    Time: 0 ms (100%), Space: 8 MB (87.55%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int factorial(int n){
+        int ans = 1;
+
+        while(n){
+            ans=ans*n;
+            n--;
+        }
+        return ans;
+    }
+    string getPermutation(int n, int k) {
+        vector<int>ans;
+        int fact = factorial(n-1);
+        int currSize=n-1;
+        vector<int>arr(n);
+
+        for(int i=0;i<n;i++) arr[i]=i+1;
+        while(ans.size()!=n){
+            int i=0;
+            while(k>fact){
+                k-=fact;
+                i++;
+                
+            }
+            ans.push_back(arr[i]);
+             arr.erase(arr.begin()+i); 
+            
+
+            if(currSize>0)
+            fact/=currSize;
+            currSize--;
+        }
+
+        string s = "";
+
+        for(int i=0;i<ans.size();i++){
+            s+=('0'+ans[i]);
+        }
+
+        return s;
+        
+    }
+};
