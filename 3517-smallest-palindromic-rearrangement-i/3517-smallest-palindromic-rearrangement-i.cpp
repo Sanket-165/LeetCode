3fead526676701caa19ab935601b349171b6dfeb
Sanket Gavhane <166587893+Sanket-165@users.noreply.github.com>
2026-07-28 11:14:04 +0530

    Time: 99 ms (29.23%), Space: 54 MB (95.38%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        string sub="";
-        for(int i=0;i<(s.length()/2);i++){
-            sub+=s[i];
-        }
-        sort(sub.begin(),sub.end());
-        int i=0;
-        int j=s.length()-1;
+       sort(s.begin(),s.begin()+(s.length()/2));
+       int i=0;
+       int j = s.length()-1;
 
-        while(i<j){
-            s[i]=sub[i];
-            s[j--]=sub[i++];
-        }
-        return s;
+       while(i<j){
+        s[j--]=s[i++];
+       }
+       return s;
     }
+
 };
