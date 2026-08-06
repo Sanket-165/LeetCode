4a6334cd31792e33f0e74a19e9f7b862a4e81274
Sanket Gavhane <166587893+Sanket-165@users.noreply.github.com>
2026-08-06 12:42:06 +0530

    Time: 2 ms (4.28%), Space: 9.2 MB (3.33%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+
+        while(true){
+            string s = to_string(n);
+            int pro=1;
+            for(int i=0;i<s.length();i++){
+                pro*=(s[i]-'0');
+            }
+            if((pro%t)==0) return n;
+
+            n++;
+         }
+        return 0;
+    }
+};
